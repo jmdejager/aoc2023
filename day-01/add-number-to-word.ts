0496c77606515d2b082a13da0d0a09dd4ebf4e60
jasperdj <jasperdj@anva.nl>
2023-12-01 11:47:14 +0100

    day-01 cleanup

--- day-01/add-number-to-word.ts
+++ day-01/add-number-to-word.ts
@@ -1,19 +1,19 @@
 const numberMap: Record<string, string> = {
-    'one': 'o1ne',
-    'two': 't2wo',
-    'three': 't3hree',
-    'four': 'f4our',
-    'five': 'f5ive',
-    'six': 's6ix',
-    'seven': 's7even',
-    'eight': 'e8ight',
-    'nine': 'n9ine',
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9',
 }
 
 export function addNumberToWord(line: string): string{
     let result = line;
     Object.entries(numberMap).forEach(([word, nr]) => {
-        result = result.replaceAll(word, nr);
+        result = result.replaceAll(word, word + nr + word);
     })
     return result;
 }
